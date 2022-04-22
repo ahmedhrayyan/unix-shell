4417dff61043afd0165263a26861fcb241dc0d86
Ahmed Hamed <ahmedhrayyan@outlook.com>
2022-04-22 05:50:16 +0200

    Execute commands in a seperate proccess

--- main.c
+++ main.c
@@ -12,7 +12,7 @@
 #define FALSE 0
 typedef int bool;
 
-int read_command(char *command, int *argc, char *argv[]);
+int read_command(int *argc, char *argv[]);
 void deallocate_args(int argc, char *argv[]);
 char *trim(char *s);
 
@@ -23,16 +23,33 @@ int main(void)
     while (TRUE)
     {
         printf("shell> ");
-        char command[MAX_LINE];
         int argc;
         char *argv[MAX_LINE / 2 + 1];
-        /* Continue incase of reading errors */
-        if (read_command(command, &argc, argv) < 0)
+        if (read_command(&argc, argv) < 0) /* Continue incase of reading errors */
             continue;
 
-        // exit shell
-        if (strcmp(command, "exit") == 0)
+        if (strcmp(argv[0], "exit") == 0) /* exit shell */
             break;
+
+        pid_t child_pid = fork();
+
+        if (child_pid < 0)
+        {
+            printf("Fork failed\n");
+            exit(1);
+        }
+        if (child_pid == 0)
+        {
+            /* Child proccess */
+            if (execvp(argv[0], argv) == -1)
+            {
+                perror("execvp");
+                exit(1);
+            }
+            exit(0);
+        }
+
+        wait(NULL);
 
         // free memory space allocated for argv
         deallocate_args(argc, argv);
@@ -40,15 +57,13 @@ int main(void)
     return 0;
 }
 
-int read_command(char *command, int *argc, char *argv[])
+int read_command(int *argc, char *argv[])
 {
-    strcpy(command, "");
     *argc = 0;
 
     char command_line[MAX_LINE];
     fgets(command_line, MAX_LINE, stdin);
 
-    // Return -1 indicating error if the input exceeded allowed length (MAX_LINE)
     if (command_line[strlen(command_line) - 1] != '\n')
     {
         printf("Exceeded allowed command length\n");
@@ -61,32 +76,26 @@ int read_command(char *command, int *argc, char *argv[])
     /* Trim whitespace from command_line */
     strcpy(command_line, trim(command_line));
 
-    /* Return -1 indicating error if user entered nothing (fgets capture new line character) */
     if (strlen(command_line) == 0)
         return -1;
 
-    /* split command_line by space saving first word in command and the reset in argv array */
     char cur_word[MAX_LINE] = "";
     for (int i = 0; i <= strlen(command_line); i++)
     {
-        /* incase of space or end of string */
         if (command_line[i] == ' ' || command_line[i] == '\0')
         {
-            if (strlen(command) == 0)
-                strcpy(command, cur_word);
-            else if (!isspace(command_line[i - 1])) /* only if the previous char was not space */
-            {
-                *argc += 1;
-                argv[*argc - 1] = (char *)malloc(MAX_LINE * sizeof(cur_word));
-                strcpy(argv[*argc - 1], cur_word);
-            }
-            /* reset cur_word */
-            strcpy(cur_word, "");
+            if (command_line[i - 1] == ' ') /* Ignore if the previous char was space */
+                continue;
+            *argc += 1;
+            argv[*argc - 1] = (char *)malloc(MAX_LINE * sizeof(cur_word));
+            strcpy(argv[*argc - 1], cur_word);
+            strcpy(cur_word, ""); /* reset cur_word */
         }
         else
-            /* Building cur_word character by character */
             strncat(cur_word, &command_line[i], 1);
     }
+
+    argv[*argc] = NULL; /* Append NULL to the end of argv (required by execvp) */
 
     return 0;
 };
